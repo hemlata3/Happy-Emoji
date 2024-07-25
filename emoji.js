a1fdc0065df5bf76fa2dea7e80c7e339d5c12192
hemlata3 <hy773733@gmail.com>
2024-07-25 21:25:51 +0530

    Add files via upload

---
+++ emoji.js
@@ -0,0 +1,11 @@
+function makehappy() {
+    let image = document.getElementById('img');
+    image.src = "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/65532/happy-emoji-clipart-md.png";
+    img.style.height = "490px";
+    img.style.width = "500px";
+    img.style.paddingLeft = "0px";
+    let button = document.getElementById('bt');
+    button.textContent = "Unhappy";
+    button.style.backgroundColor = "orange";
+    
+}
